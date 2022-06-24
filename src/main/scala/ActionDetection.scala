a4ab0ebd2a2058fbc2728016a46472b67c794267
Your Name <you@example.com>
2022-06-24 20:40:46 +0900

    2022/06/23 snap

--- src/main/scala/ActionDetection.scala
+++ src/main/scala/ActionDetection.scala
@@ -17,9 +17,9 @@ import com.example.pf.Tensor
     val fb = FitbitDataStream("src/main/resources/tetsu.sato.toml")
     //val startDateTime = s"${startDate}T00:00"
     //val endDateTime = s"${endDate}T00:00"
-    val startTime = "00:30"
-    //val startTime = "07:00"
-    //val endTime = "01:59"
+    val startTime = "00:00"
+    //val startTime = "18:00"
+    //val endTime = "03:59"
     val endTime = "23:59"
 
     val duration = 5
@@ -59,16 +59,20 @@ import com.example.pf.Tensor
             val (sampledSleepTime, sampledSleepData) = generateSampledStateStream(sampledPredictionData.time, sampledPredictionData.data, 
                                        dataSeries.sleepXData.toArray, ds, 1.0)
             NpyFile.write(Paths.get(s"heartrate-sampled-sleep-${startDate}.npy"), 
-                 sampledSleepTime.map(f => f.toDouble).toArray ++ sampledSleepData.toArray,
-              Array(2, sampledSleepTime.length.toInt))
+                 sampledSleepTime.map(f => f.toDouble).toArray ++ 
+                       sampledSleepData.getColumn(0).toArray ++
+                       sampledSleepData.getColumn(1).toArray,
+              Array(3, sampledSleepTime.length.toInt))
         if dataSeries.activeXData.length > 0 then
             val activeIter = dataSeries.activeXData.iterator
             val sampledTimeIter = sampledPredictionData.time.tensorIterator
             val (sampledActiveTime, sampledActiveData) = generateSampledStateStream(sampledPredictionData.time, sampledPredictionData.data,
                                        dataSeries.activeXData.toArray, ds, 2.0)
             NpyFile.write(Paths.get(s"heartrate-sampled-active-${startDate}.npy"), 
-                 sampledActiveTime.map(f => f.toDouble).toArray ++ sampledActiveData.toArray,
-              Array(2, sampledActiveTime.length.toInt))
+                 sampledActiveTime.map(f => f.toDouble).toArray ++ 
+                          sampledActiveData.getColumn(0).toArray ++
+                          sampledActiveData.getColumn(1).toArray   ,
+              Array(3, sampledActiveTime.length.toInt))
                                        
         NpyFile.write(Paths.get(s"heartrate-pf-${startDate}.npy"), 
               ds.timePredictStream.time.toArray.zip(ds.timePredictStream.data.toArray)
@@ -101,13 +105,17 @@ def getHeartRatePointCloud(dataSeries: DataSeries, z: Double) =
     pd.toArray
 
 def generateSampledStateStream(time: Tensor, data: Tensor, action: Array[Int], ds: DataStream, id: Double) =
+    //Logger.debug("data shape={}([:, 1] or [:, 2]?)", data.shape.toSeq)
+    //Logger.debug("data {}", data)
+    if time.length != data.shape(0) then
+        Logger.error("time length and data length must be equal. time={}, data={}", time.length, data.shape.toSeq)
     val actionIter = action.iterator
     val sampledTimeIter = time.tensorIterator
     var sampledIndex = 0
     var actionTime = actionIter.next
     var sampledTime = sampledTimeIter.next.apply(0).toInt
     val sampledActionTime = scala.collection.mutable.ArrayBuffer.empty[Int]
-    val sampledActionData = scala.collection.mutable.ArrayBuffer.empty[Double]
+    val sampledActionData = Tensor.create(0, data.shape(1).toInt)
 
     while sampledTimeIter.hasNext && actionIter.hasNext do
         //Logger.debug("actionTime: {}, sampledTime: {}", actionTime, sampledTime)
@@ -115,7 +123,12 @@ def generateSampledStateStream(time: Tensor, data: Tensor, action: Array[Int], d
                  case (action, sample) if action == sample =>
                                           ds.timeStateStream.data(sampledIndex) = id
                                           sampledActionTime += sample
-                                          sampledActionData += data(sampledIndex)
+                                          //Logger.debug("sampledActionData={}", sampledActionData)
+                                          //Logger.debug("sampledActionData.shape={}", sampledActionData.shape.toSeq)
+                                          //Logger.debug("data.getRow(sampledIndex)={}", data.getRow(sampledIndex))
+
+                                          //Logger.debug("data.getRow(sampledIndex).shape={}", data.getRow(sampledIndex).shape.toSeq)
+                                          sampledActionData.push(data.getRow(sampledIndex))
                                           //Logger.debug("sampledIndex({}) = {}", sampledIndex, id)
                                           if actionIter.hasNext then
                                               actionTime = actionIter.next
@@ -125,11 +138,11 @@ def generateSampledStateStream(time: Tensor, data: Tensor, action: Array[Int], d
                  case (action, sample) if action < sample =>
                                             if actionIter.hasNext then
                                                 actionTime = actionIter.next
-                                                Logger.debug("now, actionTime={}", actionTime)
+                                                //Logger.debug("now, actionTime={}", actionTime)
                                                 if actionTime > sampledTime then
                                                     ds.timeStateStream.data(sampledIndex) = id
                                                     sampledActionTime += sample
-                                                    sampledActionData += data(sampledIndex)
+                                                    sampledActionData.push(data.getRow(sampledIndex))
                                                     //Logger.debug("sampledIndex({}) = {}", sampledIndex, id)
                  case (action, sample) if action > sample =>
                                             if sampledTimeIter.hasNext then
--- src/test/scala/GraphicsSpec.scala
+++ src/test/scala/GraphicsSpec.scala
@@ -3,12 +3,21 @@ import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
 import scala.swing._
 import java.awt.{Color, Point}
+import breeze.plot.{Figure, Plot, plot}
 
 class GraphicsSpec extends AnyFunSuite:
     test("points should be plotted as swing objects"){
         val ui = new UI
         val xy = Array(Point(10,10), Point(10, 20), Point(20,20))
         ui.flush(xy)
+    }
+    test("breeze should be performed"){
+        val f = Figure(s"Particle filter")
+        f.width = 1480
+        f.height = 740
+        val p0 = f.subplot(3, 2, 1)
+
+        p0 += plot(Array(0.0, 1.0, 2.0), Array(0.0, 1.0, 4.0), name = "Input", style = '+', colorcode = "255,0,0")
     }
 /*
     test("points should be plotted as matplotlib objects"){
