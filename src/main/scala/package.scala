dcc64f6b93de2e3e372a58b2f8dec02403142da2
Your Name <you@example.com>
2022-08-12 19:13:04 +0900

    introduce package object

---
+++ src/main/scala/package.scala
@@ -0,0 +1,54 @@
+package org.example
+import org.tinylog.Logger
+import com.github.psambit9791.jdsp.misc.{Plotting, UtilMethods}
+import scala.jdk.CollectionConverters._
+import com.github.nscala_time.time.Imports._
+import org.joda.time.Seconds
+import org.jetbrains.bio.npy._
+import java.nio.file.Paths
+import com.example.pf.DataStream
+import scala.collection.mutable.ArrayBuffer
+import com.example.pf.Tensor
+import com.electronwill.nightconfig.core.CommentedConfig    
+import com.electronwill.nightconfig.toml.TomlParser
+import better.files.File 
+import java.nio.file.NoSuchFileException
+import java.lang.NullPointerException
+import com.electronwill.nightconfig.core.io.ParsingException
+
+def calculateSeconds(startDate: DateTime, endDate: DateTime) =
+    val interval = Seconds.secondsBetween(startDate, endDate)
+    interval
+
+var params: CommentedConfig = _
+
+def readProperties = 
+    if params == null then 
+       Logger.debug("params is null({}). try to read configuration...", params)
+       val prop = scala.sys.props.get("property.file")    
+       val tomlParser = TomlParser() 
+        params = prop match
+            case Some(filename) =>
+                 try
+                     val resource =  File(filename).contentAsString
+                     val params = tomlParser.parse(resource)
+                     params
+                 catch
+                   case e: NoSuchFileException => Logger.error(e)
+                                                  null
+            case None =>
+                 val resource = this.getClass.getClassLoader.getResourceAsStream("ActionDetection.toml") 
+                 Logger.debug("resource={}", resource)
+                 try
+                     val params = tomlParser.parse(resource)  
+                     params    
+                 catch
+                   case e: NoSuchFileException => Logger.error(e)
+                                            null
+                   case e: NullPointerException => Logger.error(e)
+                                            null
+                   case e: ParsingException => Logger.error(e)
+                                            null
+    else
+        Logger.debug("params({}) is already set", params)
+    params
---
+++ src/test/scala/packageSpec.scala
@@ -0,0 +1,35 @@
+package org.example
+import breeze.plot.{Figure, Plot, plot}
+import com.example.pf.model.{LinearGaussianObservationModel, LinearGaussianSystemModel}
+import com.example.pf.Tensor
+import org.scalactic.*
+import org.scalactic.Tolerance.*
+import org.scalatest.*
+import org.scalatest.flatspec.*
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.*
+import org.tinylog.Logger
+import com.github.nscala_time.time.Imports._
+
+class packageSpec extends AnyFunSuite:
+    test("Basic function of readProperties should be constructed") {
+        val prop = readProperties
+        Logger.debug("prop = {}", prop)
+        assert(prop != null)
+    }
+    test("Date-time information should be calculated") {
+        val prop = readProperties
+        val startDate = prop.get("StartDate").asInstanceOf[String]
+        val startTime = prop.get("StartTime").asInstanceOf[String]
+        val endDate = prop.get("EndDate").asInstanceOf[String]
+        val endTime = prop.get("EndTime").asInstanceOf[String]
+        val processStartDateObj = DateTime.parse(s"${startDate}T${startTime}")
+        val processEndDateObj = DateTime.parse(s"${endDate}T${endTime}")
+        val interval = new Interval(processStartDateObj, processEndDateObj)
+        //Logger.debug("Duration Object={}", interval.toDuration.getStandardDays)
+        //Logger.debug("Duration Object={}", interval.toDuration.getStandardHours)
+        //Logger.debug("Period Object={}", interval.toPeriod.getDays)
+        //Logger.debug("Period Object={}", interval.toPeriod.getHours)
+        assert(interval.toDuration.getStandardDays == 4)
+        assert(interval.toDuration.getStandardHours == 119)
+    }
